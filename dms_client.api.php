793c8396752833417e6b03f67ef48038b12b0a4a
dleras <david.leras@actency.fr>
2015-08-24 17:54:38 +0200

    restructuration of the architecture of the module

---
+++ dms_client.api.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 
+ */
+function hook_dms_client_admin_form() {
+	$form = array();
+	
+	return $form;
+}
+
+/**
+ * Callback function that add the plugin (hook) report in the json returned to the server
+ * 
+ * @return multitype:
+ */
+function hook_dms_client_report() {
+	return array();
+}
