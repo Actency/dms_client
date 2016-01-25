920129d740dfdd99ce23407ba869006ab321d898
Renrhaf <quentin.fahrner@actency.fr>
2016-01-25 09:56:49 +0100

    update script

--- scripts/serverinfo.sh
+++ scripts/serverinfo.sh
@@ -32,34 +32,32 @@ else
 fi
 
 # Get cpus usage
-cpuusage=`top -bn1 | awk '/Cpu/ { cpu = 100 - $8 "%" }; END   { print cpu }'`
+cpuusage=`top -bn1 | awk '/Cpu/ { cpu = 100 - $8 " %" }; END   { print cpu }'`
 
 # Get load averages
 loadavg=`uptime | awk -F'load average:' '{ print $2 }'` 2> /dev/null
+# Remove leading whitespace from load averages
+loadavg=`top -bn1 | grep load | awk '{printf "%.2f\n", $(NF-2)}'`
 
 # Get disk usage
-diskusage=`df -h | awk '$NF=="/"{printf "%d/%dGB", $3,$2,$5}'`
-
-# Remove leading whitespace from load averages
-#loadavg=`echo $loadavg | sed 's/^ *//g'`
-loadavg=`top -bn1 | grep load | awk '{printf "%.2f\n", $(NF-2)}'`
+diskusage=`df -h / | awk '$NF=="/"{printf "%d / %d GB", $3,$2,$5}' | sed "s/^/\"/; s/$/\"/;"`
+diskusage="["$diskusage"]"
 
 # Get total memory
 if [ -f "/proc/meminfo" ]; then
-    #memory=`cat /proc/meminfo | grep 'MemTotal:' | awk {'print $2}'` 2> /dev/null
-    #memory=`free -m | awk 'NR==2{printf "%s/%sMB (%.2f%)\n", $3,$2,$3*100/$2 }'`
     totalram=`free -m | awk '/Mem/ {mem=$2};END {print mem}'`
     freeram=`free -m | awk '/buffers/ {mem=$4};END {print mem}'`
     usedram=`expr $totalram - $freeram`
     unit=MB
-    memoryusage=$usedram/$totalram$unit
+    memoryusage="$usedram / $totalram $unit"
 else
     memoryusage=""
 fi
 
 # Get ip addresses using ip
 if [ -n `command -v ip` ]; then
-    ips=`ip addr show | awk -F "/" '/inet .*\//{ gsub(/inet |^[ \t]+/, "", $1); if ($1 != "127.0.0.1") print $1 }'`
+    ips=$(for i in `ip addr show | awk -F "/" '/inet .*\//{ gsub(/inet |^[ \t]+/, "", $1); if ($1 != "127.0.0.1") print $1 }' | sed "s/^/\"/; s/$/\"/;"`; do echo -n "${i}, "; done| sed 's/, $//');
+    ips="["$ips"]"
 # Try using ifconfig instead
 else
     ips=`ifconfig | awk -F "[: ]+" '/inet addr:/ { if ($4 != "127.0.0.1") print $4 }'` 2> /dev/null
@@ -70,4 +68,4 @@ if [ -z "${ips}" ]; then
     ips=`python -c 'import socket; print socket.gethostbyname(socket.gethostname())'` 2> /dev/null
 fi
 
-echo -n '{"hostname": "'$hostname'", "distrib": "'$distrib'", "uptime": "'$uptime'", "cpusnumber": '$cpusnumber', "cpuusage": '$cpuusage', "loadavg": "'$loadavg'", "memoryusage": '$memoryusage', "diskusage": '$diskusage', "ips": "'$ips'"}'
+echo -n '{"hostname": "'$hostname'", "distrib": "'$distrib'", "uptime": "'$uptime'", "cpusnumber": '$cpusnumber', "cpuusage": "'$cpuusage'", "loadavg": "'$loadavg'", "memoryusage": "'$memoryusage'", "diskusage": '$diskusage', "ips": '$ips'}'
